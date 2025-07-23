7c55d83cf3672fcec8f7b341e7528b0488848035
kulnor <pascal@codata.org>
2025-07-23 14:06:36 -0600

    Refactor test_instance_variable_foo1 to include InternationalRegistrationDataIdentifier and improve URI handling

--- tests/test_ddicdi_sempyro.py
+++ tests/test_ddicdi_sempyro.py
@@ -1,8 +1,16 @@
 from rdflib import URIRef
-from dartfx.ddi.ddicdi_sempyro import InstanceVariable, ObjectName
+from dartfx.ddi.ddicdi_sempyro import Identifier, InstanceVariable, InternationalRegistrationDataIdentifier, ObjectName
 import uuid
 
 def test_instance_variable_foo1():
     var = InstanceVariable(name = [ObjectName(name="Foo")])
+    uri = f"http://example.org/{uuid.uuid4()}"
+    irid = InternationalRegistrationDataIdentifier(
+        dataIdentifier=uri, 
+        registrationAuthorityIdentifier= "http://example.org/authority", 
+        versionIdentifier= "1.0.0")
+    identifier = Identifier(ddiIdentifier=irid)
+    var.identifier = identifier
     assert var
-    print(var.to_graph(URIRef(f"http://example.org/{uuid.uuid4()}")).serialize(format='turtle'))
+    print(var.to_graph(URIRef(uri)).serialize(format='turtle'))
+
