1f1d73f43b5016a14203cf9907eb6d320f2b1ec5
kulnor <pascal@codata.org>
2025-10-14 12:23:24 -0600

    Add tests for InstanceVariable and enhance imports in test_ddicdi_sempyro.py

--- tests/test_ddicdi_sempyro.py
+++ tests/test_ddicdi_sempyro.py
@@ -1,5 +1,5 @@
 from rdflib import URIRef
-from dartfx.ddi.ddicdi.sempyro_model import Identifier, InstanceVariable, InternationalRegistrationDataIdentifier, ObjectName
+from dartfx.ddi.ddicdi.sempyro_model import Identifier, InstanceVariable, InternationalRegistrationDataIdentifier, LabelForDisplay, LanguageString, ObjectName
 import uuid
 
 def test_instance_variable_foo():
@@ -14,3 +14,27 @@ def test_instance_variable_foo():
     assert var
     print(var.to_graph(URIRef(uri)).serialize(format='turtle'))
 
+
+def test_instance_variable_uses_concept():
+    instance_variable_id = "foo"
+    instance_variable_identifier =  Identifier(
+        ddiIdentifier=InternationalRegistrationDataIdentifier(
+            dataIdentifier=instance_variable_id,
+            registrationAuthorityIdentifier="http://example.org/authority",
+            versionIdentifier="1.0.0",
+        )
+    )
+    display_label_list: list[LabelForDisplay] = []
+    display_label_list.append(LabelForDisplay(languageSpecificString=[LanguageString(content="Foo Label")]))
+
+    instance_variable = InstanceVariable(
+        identifier=instance_variable_identifier,
+        name=[ObjectName(name="Foo")],
+        displayLabel=display_label_list,
+        uses_Concept=[URIRef("http://example.org/concept/foo")]
+    )
+    assert instance_variable
+
+    g = instance_variable.to_graph(URIRef("http://example.org/variable/foo"))
+    
+    print(g.serialize(format='turtle'))
