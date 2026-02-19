0fb12ca2360e80ec7537f8bef6137be2cd43adeb
kulnor <pascal@codata.org>
2026-02-19 11:12:37 -0700

    Refactor code structure for improved readability and maintainability

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+import pytest
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    dotenv_path = (
+        Path(__file__).parent / "../.env"
+    )  # Construct path from current test file dir
+    load_dotenv(dotenv_path=dotenv_path)
