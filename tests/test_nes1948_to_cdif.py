469ecd52d5035cabc19f9eda468a56e2d9a37116
kulnor <pascal@codata.org>
2026-03-05 13:37:13 -0700

    refactor: Standardize rdf_utils import path in test files.

--- tests/test_nes1948_to_cdif.py
+++ tests/test_nes1948_to_cdif.py
@@ -1,10 +1,10 @@
 import os
 
+import dartfx.rdf.utils as rdf_utils
 from dartfx.ddi import ddicodebook
 from dartfx.ddi.ddicdi import model_1_0_0 as model
 from dartfx.ddi.ddicdi import utils as cdi_utils
 from dartfx.ddi.ddicodebook import utils as cb_utils
-from dartfx.rdf import utils as rdf_utils
 from dartfx.rdf.pydantic import skos
 
 
